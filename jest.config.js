9f02edc454e7f364efe39c8f9afb0d0efd3f369d
Alex Kunzli <alex.kunzli@thetechcompany.ch>
2026-04-28 22:42:39 +0200

    junit

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+  coverageReporters: ['clover', 'json', 'lcov', 'cobertura'],
+  coverageDirectory: `coverage`,
+
 };
